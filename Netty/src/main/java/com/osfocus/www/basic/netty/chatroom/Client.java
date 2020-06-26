fc67feaca077a195e5f35db2d55aed719085e06b
Adrian LIU <adrian.liu.work@gmail.com>
2020-06-27 03:07:53 +1000

    Updated: Netty(Changed to Singleton ClientFrame)

--- Netty/src/main/java/com/osfocus/www/basic/netty/chatroom/Client.java
+++ Netty/src/main/java/com/osfocus/www/basic/netty/chatroom/Client.java
@@ -13,13 +13,15 @@ import io.netty.util.ReferenceCountUtil;
 public class Client {
     private final static String SERVER = "localhost";
     private final static int PORT = 8088;
-    private Channel c;
+    private Channel channel;
 
     private EventLoopGroup group = null;
 
-    ClientFrame frame = null;
+    public Client() {
 
-    public Client(ClientFrame frame) {
+    }
+
+    public void connect() {
         group = new NioEventLoopGroup(1);
 
         Bootstrap b = new Bootstrap();
@@ -31,34 +33,26 @@ public class Client {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ChannelPipeline pipeline = ch.pipeline();
-                            pipeline.addLast(new ClientHandler(frame));
+                            pipeline.addLast(new ClientHandler());
                         }
                     })
                     .connect(SERVER, PORT);
 
+            f.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (!future.isSuccess()) {
+                        System.out.println("not connected!");
+                    } else {
+                        System.out.println("connected!");
+                        channel = future.channel();
+                    }
+                }
+            });
+
             f.sync();
-            c = f.channel();
-            System.out.println("Connected to server(" + SERVER + ").");
-            this.frame = frame;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            group.shutdownGracefully();
-        }
-    }
 
-    public void sendToServer(String msg) {
-        if (c == null) {
-            throw new NullPointerException();
-        }
-
-        c.writeAndFlush(Unpooled.copiedBuffer(msg.getBytes()));
-    }
-
-    public void shutdown() {
-        try {
-            if (c != null) {
-                c.closeFuture().sync();
-            }
+            f.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -66,31 +60,43 @@ public class Client {
         }
     }
 
-    private static class ClientHandler extends ChannelInboundHandlerAdapter {
-        private final ClientFrame frame;
-
-        public ClientHandler(ClientFrame frame) {
-            this.frame = frame;
+    public void sendToServer(String msg) {
+        if (channel == null) {
+            throw new NullPointerException();
         }
+        channel.writeAndFlush(Unpooled.copiedBuffer(msg.getBytes()));
+    }
 
-        @Override
-        public void channelActive(ChannelHandlerContext ctx) throws Exception {
-            super.channelActive(ctx);
-            ByteBuf buf = Unpooled.copiedBuffer("Hello".getBytes());
-            ctx.writeAndFlush(buf);
-        }
-
-        @Override
-        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-            ByteBuf buf = null;
-            try {
-                buf = (ByteBuf) msg;
-                byte[] bytes = new byte[buf.readableBytes()];
-                buf.getBytes(buf.readerIndex(), bytes);
-                frame.setText(new String(bytes));
-            } finally {
-                if (buf != null) ReferenceCountUtil.release(buf);
+    public void shutdown() {
+        try {
+            if (channel != null) {
+                channel.closeFuture().sync();
             }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
+
+class ClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ByteBuf buf = Unpooled.copiedBuffer("Hello".getBytes());
+        ctx.writeAndFlush(buf);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf) msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(), bytes);
+            ClientFrame.INSTANCE.setText(new String(bytes));
+        } finally {
+            if (buf != null) ReferenceCountUtil.release(buf);
         }
     }
 }
--- Netty/src/main/java/com/osfocus/www/basic/netty/chatroom/ClientFrame.java
+++ Netty/src/main/java/com/osfocus/www/basic/netty/chatroom/ClientFrame.java
@@ -3,28 +3,31 @@ package com.osfocus.www.basic.netty.chatroom;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class ClientFrame extends Frame {
+    public static final ClientFrame INSTANCE = new ClientFrame();
+
     TextArea ta = new TextArea();
     TextField tf = new TextField();
     Client client = null;
 
-    public ClientFrame() {
+    private ClientFrame() {
         this.setSize(600, 400);
         this.setLocation(100, 20);
         this.add(ta, BorderLayout.CENTER);
         this.add(tf, BorderLayout.SOUTH);
-        client = new Client(this);
+
         tf.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 client.sendToServer(tf.getText());
             }
         });
+    }
 
-        this.setVisible(true);
+    public void connectToServer() {
+        client = new Client();
+        client.connect();
     }
 
     public void setText(String msg) {
@@ -33,6 +36,8 @@ public class ClientFrame extends Frame {
     }
 
     public static void main(String[] args) {
-        new ClientFrame();
+        ClientFrame frame = ClientFrame.INSTANCE;
+        frame.setVisible(true);
+        frame.connectToServer();
     }
 }
